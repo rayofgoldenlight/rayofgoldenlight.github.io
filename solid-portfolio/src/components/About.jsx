ecc723e5f1ea6082259eef3275b904b98fe2a90e
Amosecrets <229020590+rayofgoldenlight@users.noreply.github.com>
2025-08-30 22:11:30 -0400

    remod 1
    changed to be simple

--- solid-portfolio/src/components/About.jsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <section id="about" class="section">
-      <h2>About Me</h2>
-      <p>Hi! I'm a web developer passionate about modern frontend technologies.</p>
-    </section>
-  );
-}
